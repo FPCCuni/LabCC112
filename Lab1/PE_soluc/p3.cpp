3de43f8d553276c5adb962039bbab7a126be3ca9
Andre <ccbic188@gmail.com>
2024-09-03 14:03:19 -0500

    actm

--- Lab1/PE_soluc/p3.cpp
+++ Lab1/PE_soluc/p3.cpp
@@ -1,11 +1,37 @@
 //Ejercicio: Completar la solución de la pregunta 3 de la Prueba de Entrada
 //siguiendo las instrucciones dadas en la pregunta.
 
+/*
+Sugerencias: Utiliza arreglos para almacenar las distancias recorridas
+y las velocidades de los autos. 
+Emplea bucles para simular cada vuelta de la carrera. 
+Organiza el código utilizando funciones 
+(por ejemplo, para calcular la nueva velocidad, actualizar la distancia recorrida
+y determinar el ganador).
+*/
 #include <iostream>
+#include <cstdlib> //srand() rand()
+#include <ctime> //time()
+
 using namespace std;
 
-int main(){
+// Escriba una función para inicializar las velocidades de los autos
+//rand() % 10 +1 (velocidad entre 1 y 10km/h) 
+// velocidades[], tamaño?
 
-	return 0;
+
+// Escriba una función para actualizar las velocidades y distancias de los autos
+// rand() % 3 -1 (cambio de velocidad eleatorio entre -1 y 1)
+// velocidad minima 1km/h
+
+
+// Escriba una función que determie al ganador
+
+
+int main() {
+
+	//Completar
+
+    return 0;
 }
 
