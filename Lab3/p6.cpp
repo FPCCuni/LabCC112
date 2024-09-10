00609448ad4065e39acda3519990ddb361b26f02
FPCCuni <164098903+FPCCuni@users.noreply.github.com>
2024-09-10 15:04:16 -0500

    Update p6.cpp

--- Lab3/p6.cpp
+++ Lab3/p6.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 //fusionar merge(a, izq,medio,der);
 //fusion(int a[], int n1, int b[], int n2, int c[]){
-void merge(int a[], int izq, int medio, int derecho){
+void merge(int a[], int izq, int medio, int der){
+    int n1 = medio - izq +1;
+    int n2 = der - medio;
+    int aIzq[n1], aDer[n2];
+    for(int i = 0; i < n1; ++i){
+        aIzq[i] = a[izq +i];
+    }
 
+    for(int i = 0; i < n2; ++i){
+        aDer[i] = a[medio +i +1];
+    }
+    //completar utilizando la pregunta p5
 }
 
 void mergeSort(int a[], int izq, int der){
