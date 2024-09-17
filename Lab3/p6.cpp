1d74f44c72861968bc9c2f0f3a49c28f6a1daa3d
Andre <ccbic188@gmail.com>
2024-09-17 12:24:13 -0500

    act170924

--- Lab3/p6.cpp
+++ Lab3/p6.cpp
@@ -4,8 +4,37 @@ using namespace std;
 
 //fusionar merge(a, izq,medio,der);
 //fusion(int a[], int n1, int b[], int n2, int c[]){
-void merge(int a[], int izq, int medio, int derecho){
+void merge(int a[], int izq, int medio, int der){
+    int n1 = medio - izq + 1 ;
+    int n2 = der -medio;
+    int ta[n1], tb[n2];
 
+    for(int i = 0; i < n1 ; i++){
+        ta[i] = a[izq + i];
+    } 
+
+    for(int i = 0; i < n2; i++){
+        tb[i] = a[medio + 1 + i];
+    } 
+
+    int i{}, j{}, k{izq};
+
+    while(i < n1 && j < n2){
+        if(ta[i] <= tb[j]){
+            a[k++] = ta[i++];
+        }else{
+            a[k++] = tb[j++];
+        }
+    }
+
+    //si sobran elementos, los agregamos
+    while(i < n1){
+        a[k++] = ta[i++];
+    }
+
+    while(j < n2){
+        a[k++] = tb[j++];
+    }
 }
 
 void mergeSort(int a[], int izq, int der){
@@ -17,8 +46,25 @@ void mergeSort(int a[], int izq, int der){
     }
 }
 
+void imprimir(int a[], int n){
+    for(int i = 0; i < n; i++){
+        cout << a[i] <<" ";
+    }
+    cout << endl;
+}
+
 
 int main(){
+
+    int a[] = {99,6,86,15,58,35,86,4,0};
+    int n = sizeof(a)/sizeof(a[0]);
+
+    cout << "Areglo original" <<endl;
+    imprimir(a,n);
+
+    cout << "Arreglo ordenado" <<endl;
+    mergeSort(a, 0, n-1);
+    imprimir(a,n);
 
     return 0; 
 }
